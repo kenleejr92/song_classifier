5dc39eda971fc7729c0e0f5a09038ee2808ca759
Alan Gee <alanhgee@gmail.com>
2017-04-25 19:21:35 -0500

    updated to work with AWS

--- create_song_sql.py
+++ create_song_sql.py
@@ -1,35 +1,56 @@
 """
-Create an SQL table for the Million Song Database
+Create an SQL table for the Million Song Database: author and title only
 	- converts NFD text to utf8
 @author - Alan
 """
-
+import hdf5_getters
+import glob
 import pymysql.cursors
 import pandas as pd
 import numpy as np
 import unicodedata
+from tqdm import tqdm
 
 # read csv file containing id, song, artist
-data = pd.read_csv('./info.csv', delimiter = ",", names=['ID','Song','Artist']);
-data = data.replace(np.nan, '', regex=True)
+# data = pd.read_csv('./info.csv', delimiter = ",", names=['ID','Song','Artist']);
+# data = data.replace(np.nan, '', regex=True)
+
+# read from hdf5 files
+glob_path = '~/dataset/MillionSongSubset/data/*/*/*/*'
+filepaths = glob.glob(glob_path)
 
 # establish connection to sql server
-connection = pymysql.connect(host='127.0.0.1',\
+connection = pymysql.connect(host='localhost',\
    user='root',password='root',db='songs')
 
 cursor = connection.cursor()
 
 # create sql table (only need to do this once)
-sql = '''CREATE TABLE songs (
+sql = '''CREATE TABLE song_titles (
 songID VARCHAR(50) PRIMARY KEY, 
-artist VARCHAR(125) DEFAULT NULL,
-title VARCHAR(125) DEFAULT NULL,
+artist VARCHAR(200) DEFAULT NULL,
+title VARCHAR(200) DEFAULT NULL,
 INDEX artist (artist)
 );'''
 cursor.execute(sql)
 connection.commit()
 
-# populate sql table with data
+for filepath in tqdm(filepaths):
+    h5 = hdf5_getters.open_h5_file_read(filepath)
+    n = hdf5_getters.get_num_songs(h5)
+
+    for row in range(n):
+        artist = hdf5_getters.get_artist_name(h5,songidx=row)
+        song_id = hdf5_getters.get_song_id(h5,songidx=row).decode('UTF-8')
+        title= hdf5_getters.get_title(h5,songidx=row)
+        artist = "".join(c for c in unicodedata.normalize('NFD', str(artist.decode("utf8"))) if unicodedata.category(c) != "Mn")
+        title = "".join(c for c in unicodedata.normalize('NFD', str(title.decode("utf8"))) if unicodedata.category(c) != "Mn")
+        
+        query = "INSERT INTO songs (songID, artist, title) VALUES ('%s', '%s', '%s')" % (song_id, artist.replace("'", ""), title.replace("'", ""))
+		cursor.execute(query)
+
+"""
+# populate sql table with data, csv version
 for index, row in data.iterrows():
 
 	artist_name = "".join(c for c in unicodedata.normalize('NFD', unicode(row['Artist'].decode("utf8"))) if unicodedata.category(c) != "Mn")
@@ -37,9 +58,10 @@ for index, row in data.iterrows():
 
 	query = "INSERT INTO songs (songID, artist, title) VALUES ('%s', '%s', '%s')" % (row['ID'], artist_name.replace("'", ""), song_name.replace("'", ""))
 	cursor.execute(query)
+"""
 
 connection.commit()
-
+h5.close()
 # close sql connection
 cursor.close()
 connection.close()
