915b8fd174b24443b8ad4f51c5ec1c1fa492cdce
Tim Mahler <timothy@azul.me>
2017-05-02 01:03:47 -0500

    modified the get data accessor file

--- src/util/data_accessor_util.py
+++ src/util/data_accessor_util.py
@@ -17,6 +17,8 @@ import mysql_util
 import pandas as pd
 import itertools
 
+from sklearn import preprocessing
+
 
 #-------------------------
 #	Globals
@@ -33,7 +35,8 @@ def get_all_data_w_lyrics_raw():
 		LEFT JOIN genres ON genres.songID = songs.track_id
 		WHERE genres.genre IS NOT NULL
 		AND genres.genre NOT LIKE 'NULL'
-		AND songs.has_lyrics = 1;"""
+		AND songs.has_lyrics = 1
+		ORDER BY genres.genre ASC, songs.id ASC;"""
 
 	return mysql_util.execute_dict_query(q)
 
@@ -43,16 +46,13 @@ def get_all_data_raw():
 		FROM songs
 		LEFT JOIN genres ON genres.songID = songs.track_id
 		WHERE genres.genre IS NOT NULL
-		AND genres.genre NOT LIKE 'NULL';"""
+		AND genres.genre NOT LIKE 'NULL'
+		ORDER BY genres.genre ASC, songs.id ASC;"""
 
 	return mysql_util.execute_dict_query(q)
 
-# Get the data and return a dataframee
-def get_all_data():
-
-	data = get_all_data_w_lyrics_raw()
-
-	# Delete fields we dont want
+# Remove fields from feature set
+def prep_data_set(data):
 	results = []
 	for d in data:
 		d.pop('id', None)
@@ -64,14 +64,183 @@ def get_all_data():
 		d.pop('song_title', None)
 		d.pop('track_id', None)
 		d.pop('energy', None)
+		d.pop('is_train_test', None)
 
 		results.append(d)
 
-	# print "\n\n"
-	df = pd.DataFrame(results)
-	# print df
+	return results
 
-	return df
+# Preprocess train and test
+def preprocess_data(train_data, test_data):
+	train_df = pd.DataFrame(train_data)
+	test_df = pd.DataFrame(test_data)
+
+	(train_X, train_Y, train_le) = separate_target_values(train_df)
+	(test_X, test_Y, test_le) = separate_target_values(test_df)
+
+	return (train_X, train_Y, train_le, test_X, test_Y, test_le)
+
+# Separate into X & Y values
+def separate_target_values(df):
+	X = df.drop(['genre'], axis = 1)
+
+	Y = df['genre']
+
+	le = preprocessing.LabelEncoder()
+	Y = pd.DataFrame(le.fit_transform(Y))
+
+	return (X, Y, le)
+
+# 
+# Split the data into test and training sets for all data
+# 
+# +-----------------------+----------+
+# | genre                 | count(*) |
+# +-----------------------+----------+
+# | alternative or folk   |    73483 |
+# | classic rock or pop   |    36075 |
+# | country               |    18114 |
+# | dance and electronica |    81183 |
+# | hip-hop/soul/r&b      |    30434 |
+# | metal                 |    18939 |
+# | pop                   |    49656 |
+# +-----------------------+----------+
+# 
+# TRAIN SIZE : 15K, 25K, 30K
+# TEST SIZE : 3K
+# 
+def split_all_data_sets(data):
+	train_data = []
+	test_data = []
+
+	alternative = 			[x for x in data if x['genre'] == "alternative or folk"]
+	classic_rock_pop = 		[x for x in data if x['genre'] == "classic rock or pop"]
+	country = 				[x for x in data if x['genre'] == "country"]
+	dance_and_electronica = [x for x in data if x['genre'] == "dance and electronica"]
+	hip_hop = 				[x for x in data if x['genre'] == "hip-hop/soul/r&b"]
+	metal = 				[x for x in data if x['genre'] == "metal"]
+	pop = 					[x for x in data if x['genre'] == "pop"]
+
+	# Train
+	train_data.extend(alternative[:30000])
+	train_data.extend(classic_rock_pop[:30000])
+	train_data.extend(country[:15000])
+	train_data.extend(dance_and_electronica[:30000])
+	train_data.extend(hip_hop[:25000])
+	train_data.extend(metal[:15000])
+	train_data.extend(pop[:30000])
+
+	# Test
+	test_data.extend(alternative[30000:33000])
+	test_data.extend(classic_rock_pop[30000:33000])
+	test_data.extend(country[15000:18000])
+	test_data.extend(dance_and_electronica[30000:33000])
+	test_data.extend(hip_hop[25000:28000])
+	test_data.extend(metal[15000:18000])
+	test_data.extend(pop[30000:33000])
+
+	print len(train_data)
+	print len(test_data)
+
+	# for d in train_data:
+	# 	q = "UPDATE songs set is_train_test = 1 where id = %d"%(d['id'])
+	# 	mysql_util.execute_query(q)
+
+	# for d in test_data:
+	# 	q = "UPDATE songs set is_train_test = 1 where id = %d"%(d['id'])
+	# 	mysql_util.execute_query(q)
+	
+	return preprocess_data(train_data, test_data)
+
+# 
+# Split the data into test and training set for data with lyrics
+# 
+# +-----------------------+----------+
+# | genre                 | count(*) |
+# +-----------------------+----------+
+# | alternative or folk   |    25810 |
+# | classic rock / metal  |    20003 |
+# | dance and electronica |    21587 |
+# | pop                   |    22363 |
+# +-----------------------+----------+
+# 
+# TRAIN SIZE : 17K
+# TEST SIZE : 3K
+# 
+def split_data_sets_w_lyrics(data):
+	train_data = []
+	test_data = []
+
+	alternative = 			[x for x in data if x['genre'] == "alternative or folk"]
+	classic_rock_metal = 	[x for x in data if x['genre'] == "classic rock or pop" or x['genre'] == "metal"]
+	dance_and_electronica = [x for x in data if x['genre'] == "dance and electronica"]
+	pop = 					[x for x in data if x['genre'] == "pop"]
+
+
+	# Train
+	train_data.extend(alternative[:17000])
+	train_data.extend(classic_rock_metal[:17000])
+	train_data.extend(dance_and_electronica[:17000])
+	train_data.extend(pop[:17000])
+
+	# Test
+	test_data.extend(alternative[17000:20000])
+	test_data.extend(classic_rock_metal[17000:20000])
+	test_data.extend(dance_and_electronica[17000:20000])
+	test_data.extend(pop[17000:20000])
+
+	return preprocess_data(train_data, test_data)
+
+# Get all data sets
+def get_all_data_sets():
+
+	# get data
+	data = get_all_data_raw()
+
+	# prep data
+	results = prep_data_set(data)
+
+	# split into training and test
+	return split_all_data_sets(results)
+
+# Get the data w lyrics
+def get_data_sets_w_lyrics():
+
+	# get data
+	data = get_all_data_w_lyrics_raw()
+
+	# prep data
+	results = prep_data_set(data)
+
+	# split into training and test
+	return split_data_sets_w_lyrics(results)
+
+# Get the data and return a dataframee
+def get_all_data():
+
+    data = get_all_data_raw()
+
+    # Delete fields we dont want
+    results = []
+    for d in data:
+        d.pop('id', None)
+        d.pop('has_lyrics', None)
+        d.pop('deleted', None)
+        d.pop('songID', None)
+        d.pop('artist_id', None)
+        d.pop('artist_name', None)
+        d.pop('song_title', None)
+        d.pop('track_id', None)
+        d.pop('energy', None)
+        d.pop('is_train_test', None)
+
+        results.append(d)
+
+    # print "\n\n"
+    df = pd.DataFrame(results)
+    # print df
+
+    return df
 
 if __name__=="__main__":
-    get_all_data()
+    print get_data_sets_w_lyrics()
