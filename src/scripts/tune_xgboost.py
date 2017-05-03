1ffd9720158ef900a3f48967955ca0766414c4cd
Andrew Stier <andrew@cerebri.com>
2017-05-03 13:45:38 -0500

    started a xgboost tuning script

--- src/scripts/tune_xgboost.py
+++ src/scripts/tune_xgboost.py
@@ -8,6 +8,8 @@ import sys, os
 sys.path.append( os.path.realpath("%s/.."%os.path.dirname(__file__)) )
 from util import data_accessor_util
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+from xgboost import XGBClassifier
 
 df = data_accessor_util.get_all_data()
 
@@ -24,9 +26,16 @@ X_test = X[~msk]
 y_train = y[msk]
 y_test = y[~msk]
 
-clf = RandomForestClassifier()
 
-clf.fit(X_train,y_train)
-preds = clf.predict(X_test)
 
-print accuracy_score(y_test, preds)
+
+param_test1 = {
+ 'max_depth':range(3,10,2),
+ 'min_child_weight':range(1,6,2)
+}
+gsearch1 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=140, max_depth=5,
+ min_child_weight=1, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'binary:logistic', nthread=4, scale_pos_weight=1, seed=27), 
+ param_grid = param_test1, scoring='roc_auc',n_jobs=4,iid=False, cv=5)
+gsearch1.fit(train[predictors],train[target])
+gsearch1.grid_scores_, gsearch1.best_params_, gsearch1.best_score_
