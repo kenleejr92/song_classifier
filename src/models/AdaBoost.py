8d6df70480b505454badd792f541baead24474bf
frUTCS16 <farzan.memarian@utexas.edu>
2017-05-03 19:01:29 -0500

    developing models

--- src/models/AdaBoost.py
+++ src/models/AdaBoost.py
@@ -70,11 +70,11 @@ print "execution time of %s was %s seconds" %(method, elasped_time)
 print "train accuracy of method %s is %s" % (method, train_accuracy)
 print "test accuracy of method %s is %s" % (method, test_accuracy)
 print classification_report(y_test, y_test_predict, target_names=classes)
-entry1 = "train accuracy of " + method + " = " + str(train_accuracy) + "\n"
-entry2 = "test accuracy of " + method + " = " + str(test_accuracy) + "\n\n\n"
+
 myFile = open("AdaBoostClassifier.txt","a")
-myFile.write(entry1)
-myFile.write(entry2)
+print >> myFile, "execution time of %s was %s seconds\n" %(method, elasped_time)
+print >> myFile, "train accuracy of method %s is %s \n" % (method, train_accuracy)
+print >> myFile, "test accuracy of method %s is %s \n" % (method, test_accuracy)
 print >> myFile, classification_report(y_test, y_test_predict, target_names=classes)
 print >> myFile, "\n\n\n"
 print >> myFile, "best params: ", grid_search.best_params_
