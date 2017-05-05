6b845d1a4f5ee3dd784095a89f8900e1a164122c
Ubuntu <ubuntu@ip-172-31-40-33.ec2.internal>
2017-05-05 20:07:25 +0000

    XGB on the server

--- src/models/XGB.py
+++ src/models/XGB.py
@@ -1,7 +1,7 @@
 """
-AdaBoosClassifer model
+xgboost model
 
-- Performs SVM to classify the data
+- Performs XGBoost to classify the data
 
 @author - Farzan Memarian
 """
@@ -19,38 +19,39 @@ from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import GridSearchCV
-
-
+from xgboost import XGBClassifier
+import PCA
 # ----------------
 #    models
 # ----------------
-from sklearn.svm import LinearSVC
+
 
 DEBUG = 1
 
-# reading the data from sql table using the get_all_data method
-
-X_train, y_train, train_le, X_test, y_test, test_le = data_accessor_util.get_all_data_sets()
-
-classes = list(test_le.classes_)
-# print test_le.inverse_transform([0, 1, 2, 3, 4, 5, 6])
-
-# Converting data to numpy
-(X_train, y_train, X_test, y_test) = data_accessor_util.convert_data_sets_to_numpy(X_train, y_train, X_test, y_test)
-
 # method used
-method = "LinearSVC"
+method = "XGBClassifier"
+
+
+# reading the data from sql table using the get_all_data method
+var_percentage = 0.99
+(X_train, y_train, train_le, X_test, y_test, test_le) = PCA.read_data_perform_pca(var_percentage)
+classes = list(test_le.classes_)
+
 
 # iterate over classifiers
 if DEBUG == 1: print "now performing: ", method
 start = time.time()
-clf = LinearSVC(\
-	penalty='l2', loss='squared_hinge', dual=True, tol=0.0001, C=1.0, multi_class='ovr', \
-	fit_intercept=True, intercept_scaling=1, class_weight=None, verbose=0, \
-	random_state=None, max_iter=1000)
+
+# fit model no training data
+
+clf = xgboost.XGBClassifier(\
+	max_depth=3, learning_rate=0.1, n_estimators=100, silent=True, objective='binary:logistic', \
+	nthread=-1, gamma=0, min_child_weight=1, max_delta_step=0, subsample=1, colsample_bytree=1, \
+	colsample_bylevel=1, reg_alpha=0, reg_lambda=1, scale_pos_weight=1, base_score=0.5, seed=0, missing=None)
 
 # use a full grid search over all parameters
-param_grid = {"C": [0.1, 1.0, 5]}
+param_grid = {"max_depth": [6,15],
+	"learning_rate": [100,200]}
 
 # run grid search
 grid_search = GridSearchCV(clf, param_grid=param_grid)
@@ -64,14 +65,14 @@ train_accuracy = accuracy_score(y_train, y_train_predict)
 end = time.time()
 elasped_time = end - start
 
-file = open("LinearSVC.txt","w")
+file = open("XGBClassifier.txt","w")
 file.close()
 print "execution time of %s was %s seconds" %(method, elasped_time)
 print "train accuracy of method %s is %s" % (method, train_accuracy)
 print "test accuracy of method %s is %s" % (method, test_accuracy)
 print classification_report(y_test, y_test_predict, target_names=classes)
 
-myFile = open("LinearSVC.txt","a")
+myFile = open("XGBClassifier.txt","a")
 print >> myFile, "execution time of %s was %s seconds\n" %(method, elasped_time)
 print >> myFile, "train accuracy of method %s is %s \n" % (method, train_accuracy)
 print >> myFile, "test accuracy of method %s is %s \n" % (method, test_accuracy)
