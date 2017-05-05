9f97e382bac0c3d5502f2d40e599d55c96c4168e
frUTCS16 <farzan.memarian@utexas.edu>
2017-05-04 23:47:01 -0500

    xgboost added

--- src/models/PCA.py
+++ src/models/PCA.py
@@ -9,6 +9,7 @@ PCA model
 
 import numpy as np
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 import pymysql.cursors
 import sys, os
@@ -20,41 +21,59 @@ from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import GridSearchCV
-import matplotlib
-import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-matplotlib.use('Agg')
+
+def print_pca_variance():
+	# reading the data from sql table using the get_all_data method
+	X_train, y_train, train_le, X_test, y_test, test_le = data_accessor_util.get_all_data_sets()
+	classes = list(test_le.classes_)
+
+	# Converting data to numpy
+	(X_train, y_train, X_test, y_test) = data_accessor_util.convert_data_sets_to_numpy(X_train, y_train, X_test, y_test)
+
+	n_features = X_test.shape[1]
+	# this block need to be used once to create the plots
+	pca = PCA(n_components=n_features)
+	pca.fit(X_train)
+	var = pca.explained_variance_ratio_
+	cumulative_var = np.cumsum(var)
+
+	# To enable creating figures on the server
+	matplotlib.use('Agg')
+	fig = plt.figure(1)
+	plt.plot(var)
+	plt.title('individual scree plot')
+	plt.xlabel('principal components')
+	plt.ylabel('proportion of variance explained')
+	plt.savefig("proportion_variance.jpg")
+	fig2 = plt.figure(2)
+	plt.plot(cumulative_var)
+	plt.title('commulative scree plot')
+	plt.xlabel('principal components')
+	plt.ylabel('commulative proportion of variance explained')    
+	plt.savefig("proportion_Variance_com.jpg")
 
 
-DEBUG = 1
 
-# reading the data from sql table using the get_all_data method
+def read_data_perform_pca(var_percentage = 0.95):
+	# reading the data from sql table using the get_all_data method
+	X_train, y_train, train_le, X_test, y_test, test_le = data_accessor_util.get_all_data_sets()
+	classes = list(test_le.classes_)
 
-X_train, y_train, train_le, X_test, y_test, test_le = data_accessor_util.get_all_data_sets()
-classes = list(test_le.classes_)
-# print test_le.inverse_transform([0, 1, 2, 3, 4, 5, 6])
+	# Converting data to numpy
+	(X_train, y_train, X_test, y_test) = data_accessor_util.convert_data_sets_to_numpy(X_train, y_train, X_test, y_test)
+	n_features = X_test.shape[1]
+	# this block need to be used once to create the plots
+	pca = PCA(n_components=n_features)
+	pca.fit(X_train)
+	var = pca.explained_variance_ratio_
+	cumulative_var = np.cumsum(var)
 
-# Converting data to numpy
-(X_train, y_train, X_test, y_test) = data_accessor_util.convert_data_sets_to_numpy(X_train, y_train, X_test, y_test)
+	# arg max returns index, add 1 b/c indices start at 0
+	percentage_retained = var_percentage
+	N_reduced = np.argmax(cumulative_var>percentage_retained)+1;
 
-
-pca = PCA(n_components=175)
-pca.fit(X_train)
-var = pca.explained_variance_ratio_
-fig = plt.figure(1)
-plt.plot(var)
-plt.title('individual scree plot')
-plt.xlabel('principal components')
-plt.ylabel('proportion of variance explained')
-plt.savefig("proportionOfVariance.jpg")
-
-commulated_var = np.zeros(175)
-for i in range(50):
-    commulated_var[i] = np.sum(var[:i])
-
-fig2 = plt.figure(2)
-plt.plot(commulated_var)
-plt.title('commulative scree plot')
-plt.xlabel('principal components')
-plt.ylabel('commulative proportion of variance explained')    
-plt.savefig("proportionOfVarianceCom.jpg")
+	pca = PCA(n_components=N_reduced)
+	X_train = pca.fit_transform(X_train)
+	X_test = pca.transform(X_test)
+	return X_train, y_train, train_le, X_test, y_test, test_le
