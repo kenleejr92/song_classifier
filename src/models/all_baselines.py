9f011a1a6d34a3bc09335bf66cc2082c30795d38
frUTCS16 <farzan.memarian@utexas.edu>
2017-05-06 23:26:58 -0500

    all_baselines.py complete and working

--- src/models/all_baselines.py
+++ src/models/all_baselines.py
@@ -4,11 +4,13 @@ baseline models
 - Each baseline classification model has been defined as a function
 - they call run_models from util module
 - run_models in tern calls pca_util to perform pca on data
+- this should be run from src folder
 
 @author - Farzan Memarian
 """
 
 import sys, os
+import numpy as np
 import time
 sys.path.append( os.path.realpath("%s/.."%os.path.dirname(__file__)) )
 from util import data_accessor_util
@@ -21,7 +23,7 @@ from xgboost import XGBClassifier
 # ----------------
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
@@ -46,12 +48,13 @@ def xgboost_func():
 	file_name = method + ".txt"
 
 	# setting the model parameters
-	clf = method_func(max_depth=3, learning_rate=0.1, n_estimators=100, silent=True, objective='multi:softmax', \
-	nthread=5, gamma=0, min_child_weight=1, max_delta_step=0, subsample=1, colsample_bytree=1, \
-	colsample_bylevel=1, reg_alpha=0, reg_lambda=1, scale_pos_weight=1, base_score=0.5, seed=0, missing=None)
+	clf = method_func(
+		max_depth=3, learning_rate=0.1, n_estimators=100, silent=True, objective='multi:softmax', 
+		nthread=5, gamma=0, min_child_weight=1, max_delta_step=0, subsample=1, colsample_bytree=1, 
+		colsample_bylevel=1, reg_alpha=0, reg_lambda=1, scale_pos_weight=1, base_score=0.5, seed=0, missing=None)
 
 	# use a full grid search over following parameters
-	param_grid = {"max_depth": [3, 10],\
+	param_grid = {"max_depth": [3, 10],
 	              "n_estimators": [100, 200]}
 
 	run_model_util.run_model(clf, param_grid, method, file_name)
@@ -67,11 +70,11 @@ def grad_boosting_func():
 	file_name = method + ".txt"
 
 	# setting the model parameters
-	clf = method_func(\
-		loss='deviance', learning_rate=0.1, n_estimators=100, subsample=1.0, \
-	    criterion='friedman_mse', min_samples_split=2, min_samples_leaf=1, \
-	    min_weight_fraction_leaf=0.0, max_depth=3, min_impurity_split=1e-07, \
-	    init=None, random_state=None, max_features=None, verbose=0, max_leaf_nodes=None, \
+	clf = method_func(
+		loss='deviance', learning_rate=0.1, n_estimators=100, subsample=1.0, 
+	    criterion='friedman_mse', min_samples_split=2, min_samples_leaf=1, 
+	    min_weight_fraction_leaf=0.0, max_depth=3, min_impurity_split=1e-07, 
+	    init=None, random_state=None, max_features=None, verbose=0, max_leaf_nodes=None, 
 	    warm_start=False, presort='auto')
 
 	# use a full grid search over following parameters
@@ -90,18 +93,17 @@ def linear_svm_func():
 	file_name = method + ".txt"
 
 	# setting the model parameters
-	clf = method_func(\
-		penalty='l2', loss='squared_hinge', dual=True, tol=0.0001, C=1.0, multi_class='ovr', \
-		fit_intercept=True, intercept_scaling=1, class_weight=None, verbose=0, \
-		random_state=None, max_iter=1000)
+	clf = method_func(
+		penalty='l2', loss='squared_hinge', dual=True, tol=0.0001, C=1.0,
+		multi_class='ovr', fit_intercept=True, intercept_scaling=1,
+		class_weight=None, verbose=0, random_state=None, max_iter=1000)
 
 	# use a full grid search over following parameters
-	param_grid = {"penalty": []}
-
+	param_grid = {"C": [0.5, 1.0, 5.0]}
 	run_model_util.run_model(clf, param_grid, method, file_name)
 
 
-def mlf_func():
+def mlp_func():
 	method = "MLPClassifier"
 	# create file_name for saving results
 	possibles = globals().copy()
@@ -112,13 +114,15 @@ def mlf_func():
 	# setting the model parameters
 	clf = method_func(
 		activation='relu', solver='adam', alpha=0.0001, 
-		batch_size='auto', learning_rate='constant', learning_rate_init=0.001, power_t=0.5, 
-		max_iter=200, shuffle=True, random_state=None, tol=0.0001, verbose=False, warm_start=False,
+		batch_size='auto', learning_rate='adaptive', learning_rate_init=0.001, power_t=0.5, 
+		max_iter=400, shuffle=True, random_state=None, tol=0.0001, verbose=False, warm_start=False,
 		momentum=0.9, nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, 
 		beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 
 	# use a full grid search over all parameters
-	param_grid = {"hidden_layer_sizes": (15,6)}
+	param_grid = {"hidden_layer_sizes": [(15,6),(40,6)],
+	              "activation": ['relu', 'logistic']
+	}
 	run_model_util.run_model(clf, param_grid, method, file_name)
 
 
@@ -138,8 +142,9 @@ def logistic_reg_func():
 		verbose=0, warm_start=False, n_jobs=1)
 
 	# use a full grid search over all parameters
-	param_grid = {"C": [0.1, 1.0, 10.0]}
-	run_model_util.run_model(clf, param_grid, method, file_name)	
+	param_grid = {"C": [0.1, 1.0, 10.0],
+	              "penalty": ['l2', 'l1']}
+	run_model_util.run_model(clf, param_grid, method, file_name)
 
 
 def random_forest_func():
@@ -152,15 +157,15 @@ def random_forest_func():
 
 	# setting the model parameters
 	clf = method_func(
-		 n_estimators=100, criterion='gini', max_depth=None, min_samples_split=2,
-		 min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', 
-		 max_leaf_nodes=None, min_impurity_split=1e-07, bootstrap=True, 
-		 oob_score=False, n_jobs=1, random_state=None, verbose=0, 
-		 warm_start=False, class_weight=None)
+		n_estimators=100, criterion='gini', max_depth=None, min_samples_split=2,
+		min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', 
+		max_leaf_nodes=None, min_impurity_split=1e-07, bootstrap=True, 
+		oob_score=False, n_jobs=1, random_state=None, verbose=0, 
+		warm_start=False, class_weight=None)
 
 	# use a full grid search over all parameters
 	param_grid = {"n_estimators": (10,100), 
-	              "max_depth": (3,10,'none'),
+	              "max_depth": (3,10),
                   "criterion": ["gini", "entropy"]}
 	run_model_util.run_model(clf, param_grid, method, file_name)
 
@@ -175,8 +180,8 @@ def AdaBoost_func():
 
 	# setting the model parameters
 	clf = method_func(
-	base_estimator=None, n_estimators=50, learning_rate=1.0, \
-	algorithm='SAMME.R', random_state=None)
+		base_estimator=None, n_estimators=50, learning_rate=1.0,
+		algorithm='SAMME.R', random_state=None)
 
 	# use a full grid search over all parameters
 	param_grid = {"n_estimators": np.arange(20,140,40),
--- src/util/run_model_util.py
+++ src/util/run_model_util.py
@@ -15,15 +15,24 @@ import sys, os
 import time
 from tqdm import tqdm
 sys.path.append( os.path.realpath("%s/.."%os.path.dirname(__file__)) )
-from util import data_accessor_util
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.model_selection import GridSearchCV
+from time import gmtime, strftime
+
+
+# ----------------
+#    user defined packages
+# ----------------
+from util import data_accessor_util
 from util import PCA
+
+
 
 def run_model(clf, param_grid, method, file_name):
 	DEBUG = 1
+
 	# reading the data from sql table using the get_all_data method
 	var_percentage = 0.95   # percentage of variance retained by PCA
 	(X_train, y_train, train_le, X_test, y_test, test_le) = \
@@ -54,10 +63,13 @@ def run_model(clf, param_grid, method, file_name):
 
 	# create the file for storing results if don't exist
 	# remove existing file and recreate 
-	file = open(file_name, "w")
+	path_to_file = "./results/" + file_name
+	file = open(path_to_file, "w")
 	file.close()
 
-	myFile = open(file_name,"a")
+	myFile = open(path_to_file,"a")
+	print >> myFile, "current time: ", time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()), "\n"
+	print >> myFile, "model used: ", method
 	print >> myFile, "percentage variance retained by PCA: %s \n" % (var_percentage)
 	print >> myFile, "execution time of %s was %s seconds\n" % (method, elasped_time)
 	print >> myFile, "train accuracy of method %s is %s \n" % (method, train_accuracy)
@@ -72,7 +84,7 @@ def run_model(clf, param_grid, method, file_name):
 	print >> myFile, "\n\n\n"
 	print >> myFile, "best estimator: \n", grid_search.best_estimator_, "\n"
 	print >> myFile, "\n\n\n"
-	print >> myFile, "best estimator: \n", grid_search.best_estimator_, "\n"
+	print >> myFile, "best score: \n", grid_search.best_score_, "\n"
 	print >> myFile, "\n\n\n"
 	print >> myFile, "number of cross-validation splits: \n", \
 						grid_search.n_splits_, "\n"
